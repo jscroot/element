a5ad236fb37265438e2afd83126f61a88ab06265
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2023-02-24 14:30:51 +0700

    removed

--- croot.js
+++ croot.js
@@ -9,12 +9,3 @@ export function setInner(id,content){
 export function addInner(id,content){
     document.getElementById(id).innerHTML += content;
 }
-
-export function getRandomColor() {
-    let letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
